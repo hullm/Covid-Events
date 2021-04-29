6d26737bf7f58e3c81e424c54396715ce33cf176
Matthew Hull <matt@hullsite.com>
2021-04-29 13:47:42 -0400

    Linked Favicon

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="favicon.ico"/>
         <link href="css/main.css" rel="stylesheet">
     </head>
     <body class="antialiased">
